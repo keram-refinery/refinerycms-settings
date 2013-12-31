be529548696fe0e6622b882f3321f716b6936186
Marek Labos <keraml@gmail.com>
2013-12-31 12:55:47 +0100

    removed unused settings dialog

--- app/assets/javascripts/refinery/admin/settings/settings.all.js
+++ app/assets/javascripts/refinery/admin/settings/settings.all.js
@@ -75,26 +75,4 @@
     };
 
 }());
-
-// Source: scripts/admin/dialogs/settings_dialog.js
-(function (refinery) {
-
-    /**
-     * @constructor
-     * @extends {refinery.admin.Dialog}
-     * @param {Object=} options
-     * @return {refinery.admin.settings.SettingsDialog}
-     */
-    refinery.Object.create({
-        objectPrototype: refinery('admin.Dialog', {
-            title: t('refinery.admin.settings_dialog_title'),
-            url_path: '/dialogs/settings'
-        }, true),
-
-        name: 'SettingsDialog',
-
-        module: 'admin.settings'
-    });
-
-}(refinery));
 }(window, jQuery));
